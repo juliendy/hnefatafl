619e35475e7589d0ae38376cab99dae7fa6af8ec
juliendy <julien.yasar@gmail.com>
2023-01-03 17:06:57 +0100

    chore: change head + title. end of day

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useState } from "react";
 import Game from "../components/game";
 
 const Home: NextPage = () => {
     const [bgColor, setBgColor] = useState("bg-red-50")
     const classNames = "h-screen w-screen" + bgColor
+
     return (
-        <div className={classNames}>
-            <div className="text-6xl text-center pt-5">HNEFATAFL</div>
-            <Game setBgColor={setBgColor} />
-        </div>
+        <>
+            <Head>
+                <title>hnefatafl</title>
+            </Head>
+            <div className={classNames}>
+                <div className="text-6xl text-center pt-5">hnefatafl</div>
+                <Game setBgColor={setBgColor} />
+            </div>
+        </>
     );
 };
 
