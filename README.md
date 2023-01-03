10ac9efee03110d32e54f95e75d5c18144c395fc
juliendy <julien.yasar@gmail.com>
2023-01-03 13:35:33 +0100

    fix: alternating pull logic

--- README.md
+++ README.md
@@ -1,34 +1,19 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# hnefatafl
 
-## Getting Started
+This is a modern implementation of the ancient game of hnefatafl. You can see it live and in action here: COMING SOON . It is written using nextjs. The game logic is expressed as a set of TS functions and the rendering takes place only using HTML and CSS.
 
-First, run the development server:
+It includes a rudimentary AI as well as a multiplayer mode using MongoDB.
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+If you'd like to contribute, feel free to reach out. Current open points include:
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Larger work items
+ - Validation of ther ruleset with someone more involved with the game itself 
+ - Building a better AI - currently, it's severly limited by the lookahead as it does a simple search. I'd love to train an agent-based model, or at least improve upon the current search-based algorithm.
+ - A further optimized game core that more efficiently can determine legality of moves and beating to improve AI speed
+ - Determine when a now moves are left for a player and declare the other player the winner
+ - Replay
+ - Testing, especially for beating rules and movement rules
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Smaller work items
+ - Settings page, including: Disable sound, AI difficulty
+ - Link preventDefault to avoid "#" at end of URL
--- components/board.tsx
+++ components/board.tsx
@@ -2,23 +2,23 @@ import { defaultStones } from '../lib/initialSetup'
 import { Stone } from '../lib/stone'
 
 export default function Board(props: {
-    stones: number[][],
-    myteam: number[],
-    handleStoneClicked: Function,
-    handleMouseOver: Function,
-    validPathInSelection: boolean,
-    selectedStone: Stone | null
+    stones: number[][];
+    myteam: number[];
+    handleStoneClicked: Function;
+    handleMouseOver: Function;
+    validPathInSelection: boolean;
+    selectedStone: Stone | null;
+    whichTeamIsOn: number;
 }) {
-
     const getProperStyle = (item: number, row: number, col: number) => {
         // basic styling
         let styles = `shadow-md duration-75 ease-in
             rounded-md md:rounded-xl lg:rounded-xl xl:rounded-xl 2xl:rounded-xl
-            `
+            `;
         // item-coding:
         // >= 0: permanent
         // < 0: temporary
-        // --> 
+        // -->
         // 0 = empty field
         // 1 = player 1: regular
         // 2 = player 2
@@ -26,18 +26,18 @@ export default function Board(props: {
         // -1 = considering move
 
         // handle colors...
-        if (item == 0) styles = styles + " bg-stone-100"
-        if (item == 1) styles = styles + " bg-emerald-200"
-        if (item == 2) styles = styles + " bg-rose-200"
-        if (item == 3) styles = styles + " bg-lime-200"
-        if (item == -1) styles = styles + " bg-stone-200"
+        if (item == 0) styles = styles + " bg-stone-100";
+        if (item == 1) styles = styles + " bg-emerald-200";
+        if (item == 2) styles = styles + " bg-rose-200";
+        if (item == 3) styles = styles + " bg-lime-200";
+        if (item == -1) styles = styles + " bg-stone-200";
 
         if (
             // if a stone is selected, rotate it a bit
-            row == props.selectedStone?.row && 
+            row == props.selectedStone?.row &&
             col == props.selectedStone?.col
         ) {
-            styles += " rotate-[25deg]"
+            styles += " rotate-[25deg]";
         }
 
         if (
@@ -45,7 +45,8 @@ export default function Board(props: {
             // and are hovering over an empty field
             // --> offer it
             item == -1
-        ) styles += " hover:scale-125"
+        )
+            styles += " hover:scale-125";
 
         if (
             // option 2: we're choosing a stone to move
@@ -54,33 +55,47 @@ export default function Board(props: {
             (
                 props.myteam.includes(item) ||
                 (item == 3 && props.myteam.includes(1))
+            ) && 
+            (
+                item == props.whichTeamIsOn ||
+                (item == 3 && props.whichTeamIsOn == 1)
             )
-        ) styles += " hover:scale-125"
+        ) styles += " hover:scale-125";
 
-        return (styles)
-    }
+        return styles;
+    };
 
     let gridClasses = `grid 
         gap-2 md:gap-4 lg:gap-4 xl:gap-5 2xl:gap-5
-        w-full h-full`
+        w-full h-full`;
 
-    if(props.stones.length == 11) gridClasses += ' grid-cols-11'
-    if(props.stones.length == 9) gridClasses += ' grid-cols-9'
+    if (props.stones.length == 11) gridClasses += " grid-cols-11";
+    if (props.stones.length == 9) gridClasses += " grid-cols-9";
 
     return (
         <div className={gridClasses}>
             {props.stones.map((row, ridx) =>
-                row.map((item, iidx) =>
+                row.map((item, iidx) => (
                     <div
-                        key={'r' + ridx + 'c' + iidx}
+                        key={"r" + ridx + "c" + iidx}
                         className={getProperStyle(item, ridx, iidx)}
-                        onClick={() => { props.handleStoneClicked({ row: ridx, col: iidx, value: item }) }}
-                        onMouseOver={() => { props.handleMouseOver({ row: ridx, col: iidx, value: item }) }}
-                    >
-
-                    </div>
-                )
+                        onClick={() => {
+                            props.handleStoneClicked({
+                                row: ridx,
+                                col: iidx,
+                                value: item,
+                            });
+                        }}
+                        onMouseOver={() => {
+                            props.handleMouseOver({
+                                row: ridx,
+                                col: iidx,
+                                value: item,
+                            });
+                        }}
+                    ></div>
+                ))
             )}
         </div>
-    )
+    );
 }
